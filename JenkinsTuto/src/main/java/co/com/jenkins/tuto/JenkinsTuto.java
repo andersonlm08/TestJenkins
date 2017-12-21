8906ef429475b8b04b8353cb6f1dba1a71813b9e
Jorge Anderson Lopez Muñoz <jorglomu@suramericana.com.co>
2017-12-21 08:11:49 -0500

    calculo no multiplo de 3

--- JenkinsTuto/src/main/java/co/com/jenkins/tuto/JenkinsTuto.java
+++ JenkinsTuto/src/main/java/co/com/jenkins/tuto/JenkinsTuto.java
@@ -2,22 +2,26 @@ package co.com.jenkins.tuto;
 
 public class JenkinsTuto {
 
-	public int calcularPrecio(int cantidad) {
-		int precio = 0;
+	public double calcularPrecio(int cantidad) {
+		double precio = 0;
 		int modulo = modulo(cantidad);
-		
+		double cantidadDividida = dividirCantidad(cantidad);
 		if(modulo == 0 ){
-			 precio = (cantidad/3) * 5000;
+			 precio =  cantidadDividida * 5000.0D;
 		}else{
-			if((cantidad/3)>1){
-				
+			if(cantidadDividida > 1.0D){
+				return (cantidad / 3) * 5000 + (modulo * 2000.0D);
 			}
-			return modulo * 2000;
+			//retorna esto solo cuando son 1 o 2
+			return modulo * 2000.0D;
 		}
 		
 		return precio;
 	}
 	
+	private double dividirCantidad(int cantidad){
+		return (cantidad/3.0D);
+	}
 	private int modulo(int cantidad){		
 		return cantidad % 3;
 	}
--- JenkinsTuto/src/test/java/co/com/jenkins/tuto/JenkinsTutoTest.java
+++ JenkinsTuto/src/test/java/co/com/jenkins/tuto/JenkinsTutoTest.java
@@ -6,34 +6,64 @@ import static org.junit.Assert.*;
 
 public class JenkinsTutoTest {
 	
-	@Test
-	public void debeCalcularPrecioMultiploTres(){
+	//@Test
+	public void debeCalcularPrecioMultiploTres5(){
+		
+		JenkinsTuto jenkinsTuto = new JenkinsTuto();
+		int cantidad = 3;
+		double precio = jenkinsTuto.calcularPrecio(cantidad);
+		
+		assertTrue("Precio correcto", precio == 5000.0D);
+	}
+	
+	//@Test
+	public void debeCalcularPrecioMultiploTres10(){
 		
 		JenkinsTuto jenkinsTuto = new JenkinsTuto();
 		int cantidad = 6;
-		int precio = jenkinsTuto.calcularPrecio(cantidad);
+		double precio = jenkinsTuto.calcularPrecio(cantidad);
 		
-		assertTrue("Precio correcto", precio == 10000);
+		assertTrue("Precio correcto", precio == 10000.0D);
 	}
 	
-	@Test
+	//@Test
 	public void debeCalcularPrecioNoMultipoTres2(){
 		
 		JenkinsTuto jenkinsTuto = new JenkinsTuto();
 		int cantidad = 1;
-		int precio = jenkinsTuto.calcularPrecio(cantidad);
+		double precio = jenkinsTuto.calcularPrecio(cantidad);
 		
-		assertTrue("Precio correcto", precio == 2000);
+		assertTrue("Precio correcto", precio == 2000.0D);
+	}
+	
+	@Test
+	public void debeCalcularPrecioNoMultipoTres4(){
+		
+		JenkinsTuto jenkinsTuto = new JenkinsTuto();
+		int cantidad = 4;
+		double precio = jenkinsTuto.calcularPrecio(cantidad);
+		
+		assertTrue("Precio correcto", precio == 7000.0D);
 	}
 	
 	@Test
 	public void debeCalcularPrecioNoMultipoTres7(){
 		
 		JenkinsTuto jenkinsTuto = new JenkinsTuto();
-		int cantidad = 2;
-		int precio = jenkinsTuto.calcularPrecio(cantidad);
+		int cantidad = 7;
+		double precio = jenkinsTuto.calcularPrecio(cantidad);
 		
-		assertTrue("Precio correcto", precio == 4000);
+		assertTrue("Precio correcto", precio == 12000.0D);
+	}
+	
+	@Test
+	public void debeCalcularPrecioNoMultipoTres8(){
+		
+		JenkinsTuto jenkinsTuto = new JenkinsTuto();
+		int cantidad = 8;
+		double precio = jenkinsTuto.calcularPrecio(cantidad);
+		
+		assertTrue("Precio correcto", precio == 14000.0D);
 	}
 	
 	

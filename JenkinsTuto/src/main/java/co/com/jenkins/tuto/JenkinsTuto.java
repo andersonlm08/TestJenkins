c1a15d59aeaa138e531a2ed3a24961aa7d76a3d6
Anderson <jolopez@personalsoft.com>
2018-01-05 13:28:46 -0500

    se modifica calculo

--- JenkinsTuto/src/main/java/co/com/jenkins/tuto/JenkinsTuto.java
+++ JenkinsTuto/src/main/java/co/com/jenkins/tuto/JenkinsTuto.java
@@ -6,11 +6,12 @@ public class JenkinsTuto {
 		double precio = 0;
 		int modulo = modulo(cantidad);
 		double cantidadDividida = dividirCantidad(cantidad);
+		
 		if(modulo == 0 ){
 			 precio =  cantidadDividida * 5000.0D;
 		}else{
 			if(cantidadDividida > 1.0D){
-				return (cantidad / 3) * 5000 + (modulo * 2000.0D);
+				return ((cantidad / 3) * 5000) + (modulo * 2000);
 			}
 			//retorna esto solo cuando son 1 o 2
 			return modulo * 2000.0D;
